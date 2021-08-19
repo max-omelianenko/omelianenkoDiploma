936645dfc8cb021affc1c01d18212b030af32cb4
Maksym Omelianenko <makvitom@gmail.com>
2021-08-19 23:39:49 +0200

    отправка формы и проверка введенных данных

--- src/modules/sendForm.js
+++ src/modules/sendForm.js
@@ -1,12 +1,17 @@
 const sendForm = () => {
     
     const modalCallback = document.querySelector('.modal-callback'),
-    formCallback = document.querySelector('form');
+    formCallback = document.querySelector('form'),
+    allInput = modalCallback.querySelectorAll('input');
     
-    const errorMessage = 'Что то пошло не так...',
-            loadMessage = 'Загрузка...',
-            successMessage = 'Спасибо! Мы скоро с вами свяжемся!';
-        
+    const errorMessage = 'Ошибка',
+            loadMessage = 'Отправка сообщения',
+            successMessage = 'Отправлено, ожидайте звонка!';
+
+    let stringModify,
+        nameData,
+        phoneData;   
+
     const postData = (body) => {
             
         return fetch ('./server.php', {
@@ -29,8 +34,18 @@ const sendForm = () => {
         event.preventDefault();
         formCallback.appendChild(statusMessage);
         statusMessage.textContent = loadMessage;
-        const formData = new FormData(formCallback);
-        
+        allInput.forEach((elem) => {
+            if (elem.getAttribute('name') === 'fio'){
+                nameData = elem.value;
+            } else if (elem.getAttribute('name') === 'tel'){
+                phoneData = elem.value;
+            }
+        });
+        const formData = {
+            name: nameData,
+            phone: phoneData
+        };
+
         postData(formData)
             .then((response) => {
                 if (response.status !== 200){
@@ -47,10 +62,52 @@ const sendForm = () => {
                 setTimeout(deleteMessage, 5000);
             });
     });
+    // ------------- Проверка полей ввода-------------
     
-    console.log('modalCallback: ', modalCallback);
-    console.log('formCallback: ', formCallback);
+    const stringTrim = (elem) => {
+        elem = elem.replace(/\-+/g, '-');
+        elem = elem.replace(/\s+/g, ' ').trim();
+        elem = elem.replace(/^\-/g, '');
+        elem = elem.replace(/\-$/g, '');
+        return elem;
+    };
 
+    const toUpper = (str) => {
+        if(str){
+            return str
+            .toLowerCase()
+            .split(' ')
+            .map(function(word) {
+                return word[0].toUpperCase() + word.substr(1);
+            })
+            .join(' ');
+        } else {
+            str = '';
+            return str;
+        }
+    };
+
+    allInput.forEach((elem) => {
+        if (elem.getAttribute('name') === 'fio'){
+            elem.addEventListener('blur', (e) => {
+                stringModify = e.target.value;
+                stringModify = stringModify.replace(/[^?!,.\-а-яё\s]/ig, '');
+                stringModify = stringTrim(stringModify);
+                stringModify = toUpper(stringModify);
+                e.target.value = stringModify;
+            });
+        } else if (elem.getAttribute('name') === 'tel'){
+            elem.setAttribute('required', true);
+            elem.addEventListener('blur', (e) => {
+                stringModify = e.target.value;
+                stringModify = stringModify.replace(/[^\-()0-9]/ig, '');
+                stringModify = stringTrim(stringModify);
+                stringModify = '+' + stringModify;
+                stringModify = stringModify.match(/\+?[78]([-()]*\d){10}/g);
+                e.target.value = stringModify; 
+            });
+        } 
+    });
 };
 
 export default sendForm;
